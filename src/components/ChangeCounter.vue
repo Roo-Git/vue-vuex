d5246b12d600f496582d95e21538ac34b2c99604
Roo-Git <robert.martinez.delafuente@gmail.com>
2022-08-08 18:12:04 +0200

    Add mapAction

--- src/components/ChangeCounter.vue
+++ src/components/ChangeCounter.vue
@@ -2,12 +2,16 @@
   <button @click="addOne">Add 1</button>
   <button @click="addTwoWithPayload">Add 2</button>
   <button @click="addFourWithAction">Add 4 async</button>
+  <button @click="increment">Add 1 with async action</button>
+  <button @click="increase({ value: 4 })">Add 4 with async action</button>
 </template>
 
 <script>
+import { mapActions } from "vuex";
 export default {
   name: "Change-counter",
   methods: {
+    ...mapActions(["increment", "increase"]),
     addOne() {
       this.$store.commit("increment");
     },
