51a5471454f6c4da1c084938dd2ffff628efbf76
Roo-Git <robert.martinez.delafuente@gmail.com>
2022-08-08 19:20:09 +0200

    add vuex folder structure

--- src/components/ChangeCounter.vue
+++ src/components/ChangeCounter.vue
@@ -1,28 +1,17 @@
 <template>
-  <button @click="addOne">Add 1</button>
-  <button @click="addTwoWithPayload">Add 2</button>
-  <button @click="addFourWithAction">Add 4 async</button>
-  <button @click="increment">Add 1 with async action</button>
-  <button @click="increase({ value: 4 })">Add 4 with async action</button>
+  <button @click="inc">Add 2</button>
+  <button @click="increase({ value: 11 })">Add 11</button>
 </template>
 
 <script>
 import { mapActions } from "vuex";
+
 export default {
-  name: "Change-counter",
   methods: {
-    ...mapActions(["increment", "increase"]),
-    addOne() {
-      this.$store.commit("increment");
-    },
-    addTwoWithPayload() {
-      this.$store.commit("increase", { value: 2 });
-    },
-    addFourWithAction() {
-      this.$store.dispatch("increase", { value: 4 });
-    },
+    ...mapActions("numbers", {
+      inc: "increment",
+      increase: "increase",
+    }),
   },
 };
 </script>
-
-<style></style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,69 +1,22 @@
 import { createStore } from "vuex";
 
-export default createStore({
-  state: {
-    isLoggedIn: false,
-  },
-  getters: {
-    userIsAuthenticated(state) {
-      return state.isLoggedIn;
-    },
-  },
-  mutations: {
-    setAuth(state, payload) {
-      state.isLoggedIn = payload.isAuth;
-    },
-  },
-  actions: {
-    login(context) {
-      context.commit("setAuth", { isAuth: true });
-    },
-    logout(context) {
-      context.commit("setAuth", { isAuth: false });
-    },
-  },
+import rootMutations from "./mutations.js";
+import rootActions from "./actions.js";
+import rootGetters from "./getters.js";
+import counterModule from "./modules/counter/index.js";
+
+const store = createStore({
   modules: {
-    counterModule: {
-      state: {
-        counter: 0,
-      },
-      getters: {
-        finalCounter(state) {
-          return state.counter;
-        },
-        normalizedCounter(state, getters) {
-          const finalCounter = getters.finalCounter;
-          if (finalCounter < 0) {
-            return 0;
-          }
-          if (finalCounter > 100) {
-            return 100;
-          }
-          return finalCounter;
-        },
-      },
-      mutations: {
-        increment(state) {
-          state.counter++;
-        },
-        increase(state, payload) {
-          state.counter = state.counter + payload.value;
-        },
-      },
-      actions: {
-        increment(context) {
-          setTimeout(function () {
-            context.commit("increment");
-          }, 2000);
-        },
-        increase(context, payload) {
-          setTimeout(function () {
-            context.commit("increase", payload);
-          }, 2000);
-        },
-      },
-    },
+    numbers: counterModule,
   },
+  state() {
+    return {
+      isLoggedIn: false,
+    };
+  },
+  mutations: rootMutations,
+  actions: rootActions,
+  getters: rootGetters,
 });
 
-// rootState, rootGetters -> Parámetros para llegar al estado root si fuese necesario desde un modulo
+export default store;
