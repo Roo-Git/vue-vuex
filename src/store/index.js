d0c75f1d609323bb1820d23ec43f06eb73c9b645
Roo-Git <robert.martinez.delafuente@gmail.com>
2022-08-08 18:54:04 +0200

    create a module

--- src/store/index.js
+++ src/store/index.js
@@ -2,49 +2,19 @@ import { createStore } from "vuex";
 
 export default createStore({
   state: {
-    counter: 0,
     isLoggedIn: false,
   },
   getters: {
-    finalCounter(state) {
-      return state.counter;
-    },
-    normalizedCounter(state, getters) {
-      const finalCounter = getters.finalCounter;
-      if (finalCounter < 0) {
-        return 0;
-      }
-      if (finalCounter > 100) {
-        return 100;
-      }
-      return finalCounter;
-    },
     userIsAuthenticated(state) {
       return state.isLoggedIn;
     },
   },
   mutations: {
-    increment(state) {
-      state.counter++;
-    },
-    increase(state, payload) {
-      state.counter = state.counter + payload.value;
-    },
     setAuth(state, payload) {
       state.isLoggedIn = payload.isAuth;
     },
   },
   actions: {
-    increment(context) {
-      setTimeout(function () {
-        context.commit("increment");
-      }, 2000);
-    },
-    increase(context, payload) {
-      setTimeout(function () {
-        context.commit("increase", payload);
-      }, 2000);
-    },
     login(context) {
       context.commit("setAuth", { isAuth: true });
     },
@@ -52,5 +22,46 @@ export default createStore({
       context.commit("setAuth", { isAuth: false });
     },
   },
-  modules: {},
+  modules: {
+    counterModule: {
+      state: {
+        counter: 0,
+      },
+      getters: {
+        finalCounter(state) {
+          return state.counter;
+        },
+        normalizedCounter(state, getters) {
+          const finalCounter = getters.finalCounter;
+          if (finalCounter < 0) {
+            return 0;
+          }
+          if (finalCounter > 100) {
+            return 100;
+          }
+          return finalCounter;
+        },
+      },
+      mutations: {
+        increment(state) {
+          state.counter++;
+        },
+        increase(state, payload) {
+          state.counter = state.counter + payload.value;
+        },
+      },
+      actions: {
+        increment(context) {
+          setTimeout(function () {
+            context.commit("increment");
+          }, 2000);
+        },
+        increase(context, payload) {
+          setTimeout(function () {
+            context.commit("increase", payload);
+          }, 2000);
+        },
+      },
+    },
+  },
 });
