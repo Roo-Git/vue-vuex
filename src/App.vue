0c4e8447ffef2bea7e4b18d1f2af020fcb105882
Roo-Git <robert.martinez.delafuente@gmail.com>
2022-08-08 16:46:36 +0200

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,27 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <base-container title="Vuex"></base-container>
 </template>
 
+<script>
+import BaseContainer from "./components/BaseContainer.vue";
+
+export default {
+  components: {
+    BaseContainer,
+  },
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  box-sizing: border-box;
 }
 
-nav {
-  padding: 30px;
+html {
+  font-family: sans-serif;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+body {
+  margin: 0;
 }
 </style>
