791e9c16125bcccc04d9e8a953da83d8051fcf91
Roo-Git <robert.martinez.delafuente@gmail.com>
2022-08-08 17:06:35 +0200

    Conecting component to state

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <template>
   <base-container title="Vuex"></base-container>
+  <the-counter></the-counter>
+  <button @click="addOne">Add 1</button>
 </template>
 
 <script>
 import BaseContainer from "./components/BaseContainer.vue";
+import TheCounter from "./components/TheCounter.vue";
 
 export default {
   components: {
     BaseContainer,
+    TheCounter,
+  },
+  methods: {
+    addOne() {
+      this.$store.state.counter++;
+    },
   },
 };
 </script>
---
+++ src/components/TheCounter.vue
@@ -0,0 +1,16 @@
+<template>
+  <h3>{{ counter }}</h3>
+</template>
+
+<script>
+export default {
+  name: "The-counter",
+  computed: {
+    counter() {
+      return this.$store.state.counter;
+    },
+  },
+};
+</script>
+
+<style></style>
